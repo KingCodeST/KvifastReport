1a12a918e6ef309c0b0ad561ee5c30ee4bf82ad6
cyfotyasi <cyfotyasi@gmail.coom>
2021-08-10 12:29:34 +0200

    update

---
+++ src/app/services.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Services } from './services';
+import { environment } from 'src/environments/environment';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicesService {
+
+
+  private apiServerUrl =environment.apiBaseUrl;
+
+
+  constructor(private http: HttpClient) { }
+
+
+  public getServicesService():Observable<Services[]>{
+    return this.http.get<Services[]>(`${this.apiServerUrl}/serve`);
+  }
+
+  public addServicesService(servicesservice:Services):Observable<Services>{
+    return this.http.post<Services>(`${this.apiServerUrl}/servicesservice`,servicesservice)
+  }
+
+  public deleteServicesService(servicesserviceId:number):Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/servicesserviceId/{id}`);
+  }
+
+  public updateServicesService(servicesservice:Services):Observable<Services>{
+    return this.http.put<Services>(`${this.apiServerUrl}/servicesserviceId/{id}`,servicesservice);
+  }
+
+
+
+
+
+}
+
+
