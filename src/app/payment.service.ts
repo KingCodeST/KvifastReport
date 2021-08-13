5783b9325af3708b97f50a1e8681712dd4705cd1
cyfotyasi <cyfotyasi@gmail.coom>
2021-08-13 10:07:20 +0200

    update

--- src/app/payment.service.ts
+++ src/app/payment.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Payment } from './payment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
 
-  constructor() { }
+  constructor(private http: HttpClient){}
+
+  public getPayment():Observable<Payment[]>{
+    return this.http.get<Payment[]>('${this.apiServerUrl}/payments');
+}
+
+public addPayment(paymentmode:Payment):Observable<Payment>{
+    return this.http.post<Payment>('${this.apiServerUrl}/payment',paymentmode);
+}
+
+public deletePayment(paymeentmodeId: number):Observable<void>{
+    return this.http.delete<void>('${this.apiServerUrl}/payment/{paymeentmodeId}');
+}
+
+public updatePayment(paymentId:Payment):Observable<Payment>{
+    return this.http.put<Payment>('${this.apiServerUrl}/payment/{paymentId}',paymentId);
+}
+
+
 }
--- src/app/paymentmode.service.ts
+++ src/app/paymentmode.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from "rxjs";
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
+import { PaymentMode } from './paymentmode';
 
 
 @Injectable({
@@ -13,6 +14,20 @@ export class PaymentmodeService {
   constructor(private http: HttpClient){}
 
 
-  
+  public getPaymentMode():Observable<PaymentMode[]>{
+             return this.http.get<PaymentMode[]>('${this.apiServerUrl}/paymentmodes');
+         }
+    
+         public addPaymentMode(paymentmode:PaymentMode):Observable<PaymentMode>{
+             return this.http.post<PaymentMode>('${this.apiServerUrl}/paymentmode',paymentmode);
+         }
+    
+         public deletePaymentMode(paymeentmodeId: number):Observable<void>{
+             return this.http.delete<void>('${this.apiServerUrl}/paymentmodes/{paymeentmodeId}');
+         }
+    
+         public updatePaymentMode(paymentmode:PaymentMode):Observable<PaymentMode>{
+             return this.http.put<PaymentMode>('${this.apiServerUrl}/paymentmodes/{id}',paymentmode);
+         }
 
 }
