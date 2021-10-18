bc2d5ba6edc58fe043405eba13fff63305794cfd
cyfotyasi <cyfotyasi@gmail.coom>
2021-10-19 01:27:01 +0200

    update

---
+++ webpack.config.ts
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin =require('html-webpack-plugin');
+
+
+module.exports ={
+    entry:'./src/main.ts',
+    resolve:{
+        extensions: ['.ts','.js']
+    },
+    module:{
+        rules:[
+            {
+                test:/\.ts$/,
+                use:['ts-loader','angular2-template-loader']
+            },
+            {
+                test:/\.(html|css)$/,
+                use:'raw-loader'
+            }
+        ]
+    },
+
+    Plugins:[
+        new HtmlWebpackPlugin({ template:'./src/index.html'})
+    ],
+    devServer:{
+        historyApiFallBack:true
+    }
+
+
+
+
+}
