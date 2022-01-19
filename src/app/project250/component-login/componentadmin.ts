24132118c4f41d6ca6cc8fce7e27061baf4352d6
cyfotyasi <cyfotyasi@gmail.com>
2022-01-19 16:44:57 +0200

    rename the files system

---
+++ src/app/project250/component-login/componentadmin.ts
@@ -0,0 +1,9 @@
+export interface ComponentAdmin{
+   
+    id:number;
+    admin:String;
+    username:String;
+    password:String;
+
+
+}
---
+++ src/app/project250/component-user/componentuser.ts
@@ -0,0 +1,14 @@
+export interface Componentuser{
+
+    id:String;
+    email:String;
+    profileid:String;
+    name:String;
+    surname:String;
+    password:String;
+    phonenumber:String;
+    roleId:String;
+    description:String;
+    isActive:boolean;
+    
+}
