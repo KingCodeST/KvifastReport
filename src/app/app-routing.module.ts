708e658b8a9941640022b05a783ac058aa80a7ee
cyfotyasi <cyfotyasi@gmail.com>
2022-02-09 10:46:18 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,9 @@ import { PaymentComponent } from './project250/payment_s/payments.component';
 import { ProfileComponent } from './project250/profile/profilee.component';
 import { ServicesComponent } from './project250/service_/service.component';
 import { OurteamsComponent } from './project250/out_team/ourteams.component';
-
+import { ComponentLoginComponent } from './project250/component-login/componentlogin.component';
+import { ComponentAdminComponent } from './project250/component-admin/componentadmin.component';
+import { ComponentUserComponent } from './project250/component-user/componentuser.component';
 
 const routes: Routes = [
 
@@ -18,7 +20,9 @@ const routes: Routes = [
   {path:'service',component:ServicesComponent},
   {path:'profile',component:ProfileComponent},
   {path:'ourteams',component:OurteamsComponent},
-
+  {path:'login',component:ComponentLoginComponent},
+  {path:'account',component:ComponentAdminComponent},
+  {path:'user',component:ComponentUserComponent},
 
  // otherwise redirect to home
  {path:'**',redirectTo:''}
