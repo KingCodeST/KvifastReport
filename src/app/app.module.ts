9f78423ef0916ba9a4c9234c8203f0331e1cc328
cyfotyasi <cyfotyasi@gmail.com>
2022-02-24 22:31:55 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurrentStatusComponent } from './project250/current_status/currentstatus.componet';
 import { ProfileComponent } from './project250/profile/profilee.component';
 import { ServicesComponent } from './project250/service_/service.component';
 import { PaymentComponent } from './project250/payment_s/payments.component';
 import { PaymentmodeComponent } from './project250/paymentmode/paymentmode.component';
-import { CurrentstatusService } from './project250/current_status/currentstatus.service';
 import { HttpClientModule } from '@angular/common/http';
 import { home } from './project250/home/home.component';
 import { OurteamsComponent } from './project250/out_team/ourteams.component';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 
 
@@ -33,11 +31,11 @@ import { FormsModule } from '@angular/forms';
 
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    
+    HttpClientModule
+    å
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/project250/component-login/componentlogin.component.ts
+++ src/app/project250/component-login/componentlogin.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ServiceloginService } from './servicelogin.service';
 import { Componentuser } from '../component-user/componentuser';
+import { Router } from '@angular/router';
+import { error } from 'console';
 
 @Component({
   selector: 'app-component-login',
@@ -11,14 +13,24 @@ import { Componentuser } from '../component-user/componentuser';
 })
 export class ComponentLoginComponent implements OnInit {
   user =new Componentuser();
+  msg ='';
 
-  constructor(private _service : ServiceloginService) { }
+  constructor(private _service : ServiceloginService,private _router: Router) { }
 
   ngOnInit(): void {
   }
 
   loginUser(){
-    //  this._service.loginUserFromRemote();
+     this._service.loginUserFromRemote(this.user).subscribe(
+       data =>{
+         console.log("response recieved");
+         this._router.navigate(['/profile'])
+       },
+       error => {
+        console.log("exception occured")
+        this.msg ="Bad credentials, please enter valid email and password";
+       }
+     )
   }
 
   registerUser(){
